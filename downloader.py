84d211652f353fe22fbb66c7b6212e79c2c97b72
thyeros <minsikcho@us.ibm.com>
2020-07-23 11:47:50 -0500

    use -O not -o

--- downloader.py
+++ downloader.py
@@ -5,6 +5,6 @@ import os
 with open('repositories.csv', newline='') as csvfile:
     gitreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in gitreader:        
-        cmd = "wget "+row[4]+" -o ./"+row[1]+"_"+row[2]+".zip"
+        cmd = "wget "+row[4]+" -O ./"+row[1]+"_"+row[2]+".zip"
         print(cmd)
         os.system(cmd)
